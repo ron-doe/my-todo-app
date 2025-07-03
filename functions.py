34462d47d846c93359665865d5233f95fdada75c
ronaldleon <ronald.leon1@ulatina.net>
2025-07-03 16:05:26 -0600

    Initial commit

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-FILEPATH = "/Users/ronaldleon/PycharmProjects/todo-app/Main_app_code/todos.txt"
+FILEPATH = "todos.txt"
 
 def get_todos(filepath=FILEPATH):
     """ Read a text file  and return list of
@@ -15,9 +15,3 @@ def write_todos(todos_arg, filepath=FILEPATH):
         file_local.writelines(todos_arg)
 
 
-def enumerate_todos():
-    for index, item in enumerate(todos, start=1):
-        """enumerate and capitalizes all to dos"""
-        item = item.capitalize()
-        item = item.strip('\n')
-        print(f"{index}- {item}")
